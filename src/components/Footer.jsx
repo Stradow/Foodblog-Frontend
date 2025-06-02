9f8b67b444cd19157adf19d43c5355461de9b5f0
Stradow <karydis_kostas@icloud.com>
2025-06-02 10:36:47 +0300

    homepage update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-white text-gray-500 text-center py-3 text-2xl">
+      © {new Date().getFullYear()} Food Blog. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,23 @@
+import CoreConcept from "./CoreConcept";
+import posts from "../data/data";
+
+function MainSection() {
+  return (
+    <main className="flex-grow bg-[#140524]">
+      <section className="p-8 rounded-md">
+        <ul className="list-none p-0 m-0 flex gap-8 flex-wrap justify-center">
+          {posts.map((post) => (
+            <CoreConcept
+              key={post.id}
+              title={post.title}
+              image={post.cover}
+              content={post.content}
+            />
+          ))}
+        </ul>
+      </section>
+    </main>
+  );
+}
+
+export default MainSection;
