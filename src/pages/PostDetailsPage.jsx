5ea94a24754ac8b1cab04890a11f011d1b66729f
Sven Kaack <sven.kaack@gmx.de>
2025-06-03 23:38:00 +0200

    applied UI layout to PostDetailspage

--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -42,18 +42,23 @@ const PostDetailsPage = () => {
   };
 
   return (
-    <div className="hero bg-base-100 min-h-screen">
+    <div className="hero bg-[#F6F2ED] min-h-screen">
       <div className="hero-content flex-col lg:flex-row">
         <img src={cover} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-5xl font-bold max-w-xl">{title}</h1>
-          <p className="py-6 max-w-xl">{content}</p>
+          <h1 className="text-[#DE3031] text-5xl font-bold max-w-xl">{title}</h1>
+          <p className="text-black py-6 max-w-xl">{content}</p>
           <div className="flex gap-2">
-            <button onClick={handleDelete} className="btn btn-primary">
+            <button
+              onClick={handleDelete}
+              className="cursor-pointer bg-red-500 px-4 py-2 rounded-md hover:bg-red-700 transition"
+            >
               Delete
             </button>
             <Link to={`/posts/${postId}/edit`}>
-              <button className="btn btn-success">Edit</button>
+              <button className="cursor-pointer bg-gray-500 px-4 py-2 rounded-md hover:bg-gray-700 transition">
+                Edit
+              </button>
             </Link>
           </div>
         </div>
