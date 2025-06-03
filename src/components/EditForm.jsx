cab35414fc8c7eda2dceead069f0062f0fb82a38
Sven Kaack <sven.kaack@gmx.de>
2025-06-03 17:07:56 +0200

    updates mainly on  edit form and  restructuring code

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { updatePost } from '../data/posts';
 import { useParams } from 'react-router';
+import { getPostById } from '../data/posts';
 
 const EditForm = () => {
   const {postId} = useParams();
     const [form, setForm] = useState({
-    imgUrl: '',
+    author: '',
     title: '',
-    content: ''
+    content: '',
+    cover: ''
   });
+
+
+  useEffect(() => {
+    let ignore = false;
+    (async () => {
+      try {
+        const postData = await getPostById(postId);
+        if (!ignore) {
+          setForm(postData);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+
+    return () => {
+      ignore = true;
+    };
+  }, [postId]);
 
   const handleChange = e => {
     setForm(prev => ({ ...prev, [e.target.name]: e.target.value }));
@@ -22,9 +43,14 @@ const EditForm = () => {
   //     }
   // };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    updatePost(postId, form);
+    try  {
+      await updatePost(postId, form);
+
+    } catch (error) {
+      console.error(error);
+    }
 
     setForm({
       imgUrl: '',
@@ -37,6 +63,17 @@ const EditForm = () => {
     <section className="max-w-3xl ml-5">
       <h2 className="mb-4 mt-4 font-bold">YOU CAN EDIT THE POST HERE:</h2>
       <form onSubmit={handleSubmit} id="edit-form" className="flex flex-col gap-4 w-3/4">
+        <label className="w-full flex gap-2 items-baseline">
+          <span className="text-l">Author:</span>
+          <input
+            value={form.author}
+            onChange={handleChange}
+            name="author"
+            type="text"
+            placeholder="Edit title..."
+            className="bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 grow"
+          />
+        </label>
         <label className="w-full flex gap-2 items-baseline">
           <span className="text-l">Title:</span>
           <input
@@ -54,6 +91,17 @@ const EditForm = () => {
             value={form.content}
             onChange={handleChange}
             name="content"
+            type="text"
+            placeholder="Edit content..."
+            className="bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 grow"
+          />
+        </label>
+        <label className="w-full flex gap-2 items-baseline">
+          <span className="text-l">Cover:</span>
+          <input
+            value={form.cover}
+            onChange={handleChange}
+            name="cover"
             type="text"
             placeholder="Edit content..."
             className="bg-inherit border-solid border-2 border-slate-700 rounded-lg p-2 grow"
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,10 +3,30 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const MainLayout = () => {
+
+  // move currPost state to MainLayout
+  // useEffect(() => {
+  //   let ignore = false;
+  //   (async () => {
+  //     try {
+  //       const postData = await getPostById(postId);
+  //       if (!ignore) {
+  //         setCurrPost(postData);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   })();
+
+  //   return () => {
+  //     ignore = true;
+  //   };
+  // }, [postId]);
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">
+        {/* pass it to outlet via outlet context */}
         <Outlet />
       </main>
       <Footer />
