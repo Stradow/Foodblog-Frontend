5f80fe10e5f7c4d2c845efdad7fcce3a005ee347
Stradow <karydis_kostas@icloud.com>
2025-06-04 13:33:02 +0300

    wrong backend fetching typo

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -17,7 +17,7 @@ function CreatePost() {
     };
 
     try {
-      const res = await fetch('http://localhost:5174/posts', {
+      const res = await fetch('https://foodblog-backend-dwk1.onrender.com/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
